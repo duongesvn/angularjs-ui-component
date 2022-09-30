de80914bfba5b4e14c3cf8e8ec52cb075ac3592a
duong esvn <duonglt@esvn.com.vn>
2022-09-30 17:34:47 +0700

    sqlite

--- sqlite/run_01.sql
+++ sqlite/run_01.sql
@@ -5,26 +5,36 @@ select
     ,a_out.amount_out
     ,a_in.amount_in - a_out.amount_out _diff
 from (
-
+    
     select 
         strftime('%Y-%m',created_date) ym
-        ,uuid_account_from acfrom
+        ,uuid_account acfrom
         ,sum(amount) amount_in
     from exchange_book
     where exchange_type = 'IN'
     group by 
         strftime('%Y-%m',created_date)
-        ,uuid_account_from 
+        ,uuid_account 
+    union 
+    select 
+        strftime('%Y-%m',created_date) ym
+        ,uuid_account_to acfrom
+        ,sum(amount) amount_in
+    from exchange_book
+    where exchange_type = 'CH'
+    group by 
+        strftime('%Y-%m',created_date)
+        ,uuid_account 
 ) a_in
 left join (
 
     select 
         strftime('%Y-%m',created_date) ym
-        ,uuid_account_from acfrom
+        ,uuid_account acfrom
         ,sum(amount) amount_out
     from exchange_book
     where exchange_type in ('OUT','CH')
     group by 
         strftime('%Y-%m',created_date)
-        ,uuid_account_from
+        ,uuid_account
 ) a_out on a_out.acfrom = a_in.acfrom and a_out.ym = a_in.ym
